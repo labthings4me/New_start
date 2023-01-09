261461a048aa801bcd9f6b8304baf9dbf549d8a2
labthings4me <zoehedstrom@yahoo.com>
2023-01-09 13:35:51 -0500

    update

---
+++ New_start/creativecoding/variables_gif/sketch.js
@@ -0,0 +1,28 @@
+// create a program that can customise the number of sqaures in a grid
+let gif;
+funstion preload () {
+  gif = loadImage( 'assets/new_selfieportrait.gif');
+
+}
+function setup() { //runs once
+ createCanvas (windowWidth,windowWidth); //creates a canvas 720px x 720 px
+ background (255,255,0);
+
+}
+
+function draw() { //runs in a loop
+//fill ('lavender'); //fils colour
+//strokeWeight (1); //assigns stroke weight
+var num = 30; ///  number of squares in my array
+var sideLen = windowWidth/num; // this is our side length
+
+
+for (var y = 0; y < windowWidth ; y = y + sideLen){ //loop creates a row in the y  direction
+for (var x = 0; x < windowWidth ; x = x + sideLen){ //loop creates a row in the x direction
+
+  image (gif,x,y,windowWidth/num,windowWidth/num,); //img loadImage
+}
+}
+
+
+}
