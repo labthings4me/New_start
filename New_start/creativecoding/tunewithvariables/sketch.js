073ff47cd3915f4e79cdf178920b16558447f80b
labthings4me <zoehedstrom@yahoo.com>
2023-01-09 09:47:53 -0500

    update

--- New_start/creativecoding/tunewithvariables/sketch.js
+++ New_start/creativecoding/tunewithvariables/sketch.js
@@ -1,19 +1,25 @@
+// create a program that can customise the number of sqaures in a grid
+
 function setup() { //runs once
- createCanvas (720,720); //creates a canvas 720px x 720 px
+ createCanvas (windowWidth,windowWidth); //creates a canvas 720px x 720 px
  background (255,255,0);
 
 }
 
 function draw() { //runs in a loop
-fill (255,0,0); //fils colour
+fill (255,255,0); //fils colour
 strokeWeight (1); //assigns stroke weight
-for (var y = 0; y < 720 ; y = y + 360){ //loop creates a row in the y  direction
-for (var x = 0; x < 720 ; x = x + 360){ //loop creates a row in the x direction
+var num = 4; ///  number of squares in my array
+var sideLen = windowWidth/num; // this is our side length
+
+
+for (var y = 0; y < windowWidth ; y = y + sideLen){ //loop creates a row in the y  direction
+for (var x = 0; x < windowWidth ; x = x + sideLen){ //loop creates a row in the x direction
 
   quad (x,y,
-        x+360,y,
-        x+360,y+360,
-         x,y+360);
+        x+sideLen,y,
+        x+sideLen,y+sideLen,
+         x,y+sideLen);
 }
 }
 
