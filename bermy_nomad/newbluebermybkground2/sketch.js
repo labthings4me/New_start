c88b96cbd8d2a3439afc3d6e0e565af85c1c0455
labthings4me <zoehedstrom@yahoo.com>
2023-01-12 20:51:48 -0500

    changes

---
+++ bermy_nomad/newbluebermybkground2/sketch.js
@@ -0,0 +1,58 @@
+
+
+
+function preload (){
+  boat = loadImage ('boat.png');
+  map = loadImage ('map.png');
+  lizard = loadImage ('lizard.png');
+  lily = loadImage ('lily.png');
+  fish = loadImage ('fish.png');
+  palm = loadImage ('palm.png');
+  cent = loadIamge ('cent.png')
+}
+  function setup() {
+    createCanvas(windowWidth, windowWidth);
+frameRate (.5);
+  }
+
+function draw() {
+  fill ('yellow');
+  strokeWeight (1);
+  background ('white');
+
+
+ var num = 6 ;
+
+  var sideLen = windowWidth/num; // x length
+
+  translate (-150, -150); // gives design a bleed effect
+
+
+  for ( var y = 0; y < 2 * windowWidth; y = y + sideLen) {
+
+    for ( var x = 0; x < 2 * windowWidth; x = x + sideLen) {//row across x
+
+
+
+    pics = [boat,map, lizard,lily,fish,palm];
+    image (pics [int (random (0,6
+    ))], x,y);
+
+
+    }
+
+  function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+
+  }
+
+  }
+
+}
+function windowResized (){ // resizes and refreshes design with browser
+resizeCanvas (windowWidth, windowHeight);
+
+
+
+
+}
